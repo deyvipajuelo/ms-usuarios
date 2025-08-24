0d2a30e82e0d2fe18f6f33ac81e75136091c027b
Deyvi Pajuelo <deyvipajuelo@hotmail.com>
2025-08-24 17:55:52 -0500

    chore: initial project structure

---
+++ src/test/java/com/pragma/powerup/DummyTest.java
@@ -0,0 +1,12 @@
+package com.pragma.powerup;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTest {
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
