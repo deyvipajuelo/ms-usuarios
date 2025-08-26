12af97da94b7166a4c85409521a09a98424d5ba0
Deyvi Pajuelo <deyvipajuelo@hotmail.com>
2025-08-26 17:11:26 -0500

    feat: add User and Role models

---
+++ src/main/java/com/pragma/powerup/domain/model/Role.java
@@ -0,0 +1,30 @@
+package com.pragma.powerup.domain.model;
+
+public class Role {
+    private Long id;
+    private String nombre;
+
+    public Role() {
+    }
+
+    public Role(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
